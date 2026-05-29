494c4d07d6893ba9d53a20254e22104016d6553e
ThapeloFx-hub <forexelitekillers@gmail.com>
2026-05-29 18:05:43 +0200

    Update btc_engine.py

--- btc_engine.py
+++ btc_engine.py
@@ -1,5 +1,22 @@
 from tradingview_ta import TA_Handler, Interval
+from flask import Flask
+from threading import Thread
 import time
+import os
+
+# FLASK SERVER
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "BTC ENGINE RUNNING"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+# START WEB SERVER
+Thread(target=run_web).start()
 
 print("BTC ENGINE STARTED")
 
