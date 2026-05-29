b92033351b1943f1b79bc5866529c628e505eaae
ThapeloFx-hub <forexelitekillers@gmail.com>
2026-05-29 17:17:38 +0200

    Update btc_engine.py

--- btc_engine.py
+++ btc_engine.py
@@ -1,1 +1,25 @@
+from tradingview_ta import TA_Handler, Interval
+import time
+
 print("BTC ENGINE STARTED")
+
+while True:
+
+    try:
+
+        btc = TA_Handler(
+            symbol="BTCUSDT",
+            screener="crypto",
+            exchange="BINANCE",
+            interval=Interval.INTERVAL_4_HOURS
+        )
+
+        analysis = btc.get_analysis()
+
+        print("BTC H4 SUMMARY:")
+        print(analysis.summary)
+
+    except Exception as e:
+        print("ERROR:", e)
+
+    time.sleep(60)
